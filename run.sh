97b14f8885dc968f97f8409fed5a1c016eb17b5f
glindste <gustaflindstedt@gmail.com>
2014-02-13 15:56:19 +0100

    bla

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cd $COMPILATROCIOUS_DIR
-make
 cd grammar_parser
 cat ../test/test000.minij | java MiniJava
 cat ../test/test001.minij | java MiniJava
+cat ../test/test002.minij | java MiniJava
