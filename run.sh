6a2b5c6abcff70949fa06ec1f32e70f18c515c39
glindste <gustaflindstedt@gmail.com>
2014-02-13 17:11:06 +0100

    more features!

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd grammar_parser
-cat ../test/test000.minij | java MiniJava
 cat ../test/test001.minij | java MiniJava
 cat ../test/test002.minij | java MiniJava
+cat ../test/test003.minij | java MiniJava
+cat ../test/test004.minij | java MiniJava
