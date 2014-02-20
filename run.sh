bde3e86380a50f51ac8d7f074cc4e6ba29917e74
JonathanMurray <jonte_murray@hotmail.com>
2014-02-20 16:08:52 +0100

    modified script and added test dirs

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd src/grammar_parser
-cat ../test/test001.minij | java MiniJava
-cat ../test/test002.minij | java MiniJava
-cat ../test/test003.minij | java MiniJava
-cat ../test/test004.minij | java MiniJava
+cat ../../test/test001.minij | java MiniJava
+cat ../../test/test002.minij | java MiniJava
+cat ../../test/test003.minij | java MiniJava
+cat ../../test/test004.minij | java MiniJava
