a3b0d14c4a57bfbf54323a2a67a3eb6f8b24af17
JonathanMurray <jonte_murray@hotmail.com>
2014-02-20 16:17:59 +0100

    more tests, test 5 and 6 are currently failing.

--- run.sh
+++ run.sh
@@ -5,3 +5,5 @@ cat ../../test/test001.minij | java MiniJava
 cat ../../test/test002.minij | java MiniJava
 cat ../../test/test003.minij | java MiniJava
 cat ../../test/test004.minij | java MiniJava
+cat ../../test/test005.minij | java MiniJava
+cat ../../test/test006.minij | java MiniJava
