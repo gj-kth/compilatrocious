354f8df1e5466ba7ecbce93e7e813fa1068bfca3
JonathanMurray <jonte_murray@hotmail.com>
2014-02-22 20:52:52 +0100

    code structure

--- src/ParseTree.java
+++ src/ParseTree.java
@@ -6,16 +6,27 @@ class ParseTree {
 
         try{
             SimpleNode parseTree = minij.Program();
-
-            System.out.println("---------------------------------------------------");
-            System.out.println("Success!!!");
-            System.out.println("----------");
-            parseTree.dump("");
-            System.out.println("---------------------------------------------------");
-
+            printSimple(parseTree);
         }catch(Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
+        }
+    }
+
+    static void printSimple(SimpleNode parseTree){
+        System.out.println("---------------------------------------------------");
+        System.out.println("Success!!!");
+        System.out.println("----------");
+        parseTree.dump("");
+        System.out.println("---------------------------------------------------");
+    }
+
+    //Currently not very useful. 
+    static void printDetails(SimpleNode parseTree){
+        System.out.println("--- " + parseTree.jjtGetValue() + " ---");
+        for(int c = 0; c < parseTree.jjtGetNumChildren(); c++){
+            Node child = parseTree.jjtGetChild(c);
+            System.out.println(child);
         }
     }
 }
