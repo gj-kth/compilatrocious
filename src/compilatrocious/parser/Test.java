d2333ab631f688277adee2e672078a36453a4413
JonathanMurray <jonte_murray@hotmail.com>
2014-03-04 21:22:15 +0100

    Improved output of tests in Test.java, and enabled negative tests with checking of thrown exceptions. Modified the test case where main was used as class name. This is not allowed atm, and is something that probably has to be fixed in the future. All tests are passing (pos and neg).

--- src/compilatrocious/parser/Test.java
+++ src/compilatrocious/parser/Test.java
@@ -1,52 +1,92 @@
 package compilatrocious.parser;
 
 import java.io.*;
+import java.util.*;
 
 public class Test {
 
-    public static final boolean PRINT = true;
+    public static final boolean PRINT = false;
 
     public static void main(String[] args) {
-         println("Testing compile ... ");
+         println("COMPILATROCIOUS_ROOT = " + System.getenv("COMPILATROCIOUS_ROOT")); 
          testCompile();
-         println("Testing noncompile ... ");
-         //testNonCompile();
+         testNonCompile();
     }
 
     public static void testCompile(){
-        System.out.println(System.getenv("COMPILATROCIOUS_ROOT"));
-        testFilesInDir(new File(System.getenv("COMPILATROCIOUS_ROOT") + "/test/compile"));
+        System.out.print("Testing compile ... ");
+        testFilesInDir(new File(System.getenv("COMPILATROCIOUS_ROOT") + "/test/compile"), true);
     }
 
     public static void testNonCompile(){
-        testFilesInDir(new File(System.getenv("COMPILATROCIOUS_ROOT") + "/test/noncompile"));   
+        System.out.print("Testing noncompile ... ");
+        testFilesInDir(new File(System.getenv("COMPILATROCIOUS_ROOT") + "/test/noncompile"), false);   
     }       
 
-    static void testFilesInDir(File dir){
-        println("Directory: " + dir.getPath());
-        File[] files = dir.listFiles();
+    private static void printFiles(List<File> files){
         for(File f : files){
-            if(f.getName().contains(".minij")){
-                testFile(f);                
-            }
+            System.out.println(f.getPath());
         }
     }
 
-    static void testFile(File testFile){
+
+    static void testFilesInDir(File dir, boolean positiveTests){
+        println("Directory: " + dir.getPath());
+        File[] files = dir.listFiles();
+        List<File> failedTests = new ArrayList<File>();
+        for(File f : files){
+            if(f.getName().contains(".minij")){
+                boolean success = testFile(f, positiveTests);                
+                if(! success){
+                    failedTests.add(f);
+                }
+            }
+        }
+
+        System.out.println("TESTS PASSED: " + (files.length - failedTests.size()) + "/" + files.length);
+    }
+
+    /**
+    * Tests to parse program in given file.
+    * (positiveTest == true) means that parsing 'should' be successful.
+    * (return == true) means that test was passed
+    */
+    static boolean testFile(File testFile, boolean positiveTest){
         println("File: " + testFile.getPath());
         try {
             InputStream is = new FileInputStream(testFile);
             ParseTree.parse(is);
             is.close(); 
-        } catch (Exception e) {
+            if(!positiveTest){
+                System.out.println(testFile.getPath());
+                System.out.println("Negative test case didn't throw exception.");
+                return false;
+            }
+        } catch (ParseException e) {
+            if(positiveTest){
+                System.out.println(testFile.getPath());
+                printException(e);
+                return false;
+            }
+        } catch(Exception e){
             e.printStackTrace();
             System.exit(0);
         }
+        return true;
     }
 
     private static void println(String str){
         if(PRINT){
             System.out.println(str);
         }
+    }
+
+    private static void printException(Exception e){
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        System.out.println("--------------------------------------------------------");
+        System.out.println(sw.toString());
+        System.out.println("--------------------------------------------------------");
     }
 }
