dbc20039eba2cbc85e27439bd88eacc967a23323
JonathanMurray <jonte_murray@hotmail.com>
2014-03-05 11:37:28 +0100

    Test now tests oneliners, all tests pass. Not sure if everything is working correctly though

--- src/compilatrocious/parser/Test.java
+++ src/compilatrocious/parser/Test.java
@@ -7,10 +7,16 @@ public class Test {
 
     public static final boolean PRINT = false;
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
          println("COMPILATROCIOUS_ROOT = " + System.getenv("COMPILATROCIOUS_ROOT")); 
-         testCompile();
-         testNonCompile();
+         try{
+            testCompile();
+             testNonCompile();
+             testNonCompileOneliners();   
+         }catch(Exception e){
+            printException(e);
+         }
+         
     }
 
     public static void testCompile(){
@@ -21,12 +27,49 @@ public class Test {
     public static void testNonCompile(){
         System.out.print("Testing noncompile ... ");
         testFilesInDir(new File(System.getenv("COMPILATROCIOUS_ROOT") + "/test/noncompile"), false);   
-    }       
+    }    
+
+    public static void testNonCompileOneliners() throws FileNotFoundException, IOException{
+        System.out.print("Testing noncompile-oneliners ...");
+        testOneliners( new File(System.getenv("COMPILATROCIOUS_ROOT") + "/test/noncompile/oneliners"), false);
+    }   
 
     private static void printFiles(List<File> files){
         for(File f : files){
             System.out.println(f.getPath());
         }
+    }
+
+   
+
+    private static void testOneliners(File fileContainingOneliners, boolean positiveTests) throws FileNotFoundException, IOException{
+        BufferedReader br = new BufferedReader(new FileReader(fileContainingOneliners));
+        String line;
+        int lineNumber = 1;
+        List<Integer> failedLines = new ArrayList<Integer>();
+        while ((line = br.readLine()) != null) {
+            String filePath = "oneliner_" + lineNumber;
+            boolean success = testOneliner(line, positiveTests, filePath);
+            if(! success){
+                failedLines.add(lineNumber);
+            }
+            lineNumber ++;
+        }
+        int numOneliners = lineNumber - 1;
+        System.out.println("TESTS PASSED: " + (numOneliners - failedLines.size()) + "/" + numOneliners);
+        br.close();
+    }
+
+    private static boolean testOneliner(String oneliner, boolean positiveTest, String fakeFilepath) throws IOException{
+        //System.out.println("Testing oneliner " + fakeFilepath); //TODO
+        //System.out.println("----------         " + oneliner + "            ------------");
+        String programText = "class test {\n"
+                           + "  public static void main (String [] args) {\n"
+                           +        oneliner + "\n"
+                           + "  }\n"
+                           + "}";
+        InputStream is = new ByteArrayInputStream(programText.getBytes());
+        return testProgram(is, positiveTest, fakeFilepath);
     }
 
 
@@ -55,22 +98,29 @@ public class Test {
         println("File: " + testFile.getPath());
         try {
             InputStream is = new FileInputStream(testFile);
-            ParseTree.parse(is);
-            is.close(); 
-            if(!positiveTest){
-                System.out.println(testFile.getPath());
-                System.out.println("Negative test case didn't throw exception.");
-                return false;
-            }
-        } catch (ParseException e) {
-            if(positiveTest){
-                System.out.println(testFile.getPath());
-                printException(e);
-                return false;
-            }
+            return testProgram(is, positiveTest, testFile.getPath());
         } catch(Exception e){
             e.printStackTrace();
             System.exit(0);
+        }
+        return false; //Will never be reached?
+    }
+
+    static boolean testProgram(InputStream programText, boolean positiveTest, String filePath) throws IOException{
+        try {
+            ParseTree.parse(programText);
+            programText.close(); 
+            if(!positiveTest){
+                System.out.println(filePath);
+                System.out.println("Negative test case didn't throw exception.");
+                return false;
+            }
+        } catch (TokenMgrError | ParseException e) {
+            if(positiveTest){
+                System.out.println(filePath);
+                printException(e);
+                return false;
+            }
         }
         return true;
     }
@@ -81,7 +131,7 @@ public class Test {
         }
     }
 
-    private static void printException(Exception e){
+    private static void printException(Throwable e){
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
         e.printStackTrace(pw);
