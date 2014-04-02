d9404a8030df47ff198a7f37f94c9061c8f2c324
glindste <gustaflindstedt@gmail.com>
2014-04-02 20:08:59 +0200

    initial move to using a hashmap internally

--- src/mjc/parser/SymbolTable.java
+++ src/mjc/parser/SymbolTable.java
@@ -6,6 +6,7 @@ public class SymbolTable implements HasPrefixedToString{
 
 	final int SIZE = 256;
 	SymbolTableBucket table[] = new SymbolTableBucket[SIZE];
+        private HashMap<Symbol,Object> map = new HashMap<Symbol,Object>();
 	
 	public SymbolTable(){
 
@@ -25,25 +26,38 @@ public class SymbolTable implements HasPrefixedToString{
 	}
 
 	void insert(Symbol s, Object b) {
+            /*
 		int index = getIndex(s);
                 if(table[index] != null) {
                     throw new DuplicateDeclaration(s, b, table[index]);
                 }
 		table[index]=new SymbolTableBucket(s, b);
+                */
+
+                if(map.get(s) != null) {
+                    throw new DuplicateDeclaration(s, b, map.get(s));
+                }
+                map.put(s,b);
 	}
 
 	Object lookup(Symbol s) {
+            /*
 		int index = getIndex(s);
 		if (table[index]!=null && s.equals(table[index].key)) 
 			return table[index].binding;
 		return null;
+                */
+            return map.get(s);
 	}
 
+        /*
 	void pop(Symbol s) {
 		int index = getIndex(s);
 		table[index]=null;
 	}
+        */
 
+        /*
 	private int getIndex(Symbol s){
 		int index=hash(s.toString())%SIZE;
 		if(index < 0 ){
@@ -51,6 +65,7 @@ public class SymbolTable implements HasPrefixedToString{
 		}
 		return index;
 	}
+        */
 
 	void insert(String s, Object b) {
 		insert(Symbol.symbol(s), b);
@@ -60,18 +75,37 @@ public class SymbolTable implements HasPrefixedToString{
 		return lookup(Symbol.symbol(s));
 	}
 
+        /*
 	void pop(String s) {
 		pop(Symbol.symbol(s));
 	}
+        */
 
 	public String toString(String prefix){
-		String s = prefix + "*SYMTABLE*";
+		StringBuilder sb = new StringBuilder(prefix + "*SYMTABLE*");
+                /*
 		for(int i = 0; i < table.length; i++){
 			if(table[i] != null){
 				s += "\n" + table[i].toString(prefix + "\t") ;	
 			}
 		}
-		return s;
+                */
+                Iterator<Symbol> it = map.keySet().iterator();
+                Symbol sym;
+                Object obj;
+                prefix = prefix + "\t"; //Move right one level
+                while(it.hasNext()) {
+                    sym = it.next();
+                    obj = map.get(sym);
+                    sb.append("\n" + prefix + sym + ": ");
+                    if(obj instanceof HasPrefixedToString){
+                            sb.append("\n" + ((HasPrefixedToString)obj).toString(prefix + "\t"));
+                    }else{
+                            sb.append("\n" + prefix + "\t" + obj);
+                    }
+                }
+
+		return sb.toString();
 	}
 
         public String toString() {
